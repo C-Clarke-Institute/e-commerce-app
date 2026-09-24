5e7ac72d0e5b1da850e11972b5ebe611437dfea2
academic.cci@gmail.com <c-clarke@c-clarkes-Mac-mini.local>
2026-09-24 15:03:16 +0530

    Angular Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from "./features/home/pages/home-page/home-page.component";
+import {ProductsPageComponent} from "./features/products/pages/products-page/products-page.component";
+import {CartPageComponent} from "./features/cart/pages/cart-page/cart-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '' , component: HomePageComponent },
+    { path: 'products', component: ProductsPageComponent },
+    { path: 'cart', component: CartPageComponent },
+];
